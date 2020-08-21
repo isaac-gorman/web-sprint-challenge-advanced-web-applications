23a225fc7aaa1a160a8f4da5e2b9556c457a81be
Isaac Gorman <emailme@isaacg.net>
2020-08-21 10:29:43 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <h4>Sprint Project</h4>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
